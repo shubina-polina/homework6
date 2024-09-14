c75323278649de90f09c5824046c2ff3c7359717
Fane Keat <02032002t@gamil.com>
2024-09-14 12:19:12 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         // задача 1
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
 // задача 2
-        for (int i = 10; i >= 0; i--) {
+        for (int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
         // задача 3
